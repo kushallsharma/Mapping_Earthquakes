00cf3127d27707955d5e6f122b1555e91be0f040
Kushal Sharma <kushalsharma.ca@gmail.com>
2021-03-28 00:06:04 -0400

    Adding changes

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,7 @@ console.log("working");
 
  
  // We create the tile layer that will be the background of our map.
- let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       // id: 'mapbox.streets',
@@ -20,15 +20,15 @@ console.log("working");
   // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [
-    30, 30
+    44.0, -80.0
   ],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
