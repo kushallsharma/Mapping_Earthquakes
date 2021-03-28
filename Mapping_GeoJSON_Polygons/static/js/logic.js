b8c3f56d07e0f721e83e4662fd8a1b07926c761d
Kushal Sharma <kushalsharma.ca@gmail.com>
2021-03-28 00:45:00 -0400

    Adding polygon code

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -2,7 +2,7 @@ console.log("working");
 
  
  // We create the tile layer that will be the background of our map.
- let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       // id: 'mapbox.streets',
@@ -10,7 +10,7 @@ console.log("working");
   });
 ;
   
-  let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
@@ -20,33 +20,38 @@ console.log("working");
   // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
   center: [
-    44.0, -80.0
+    43.7, -79.3
   ],
-  zoom: 2,
-  layers: [light]
+  zoom: 11,
+  layers: [satelliteStreets]
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Light: light,
-  Dark: dark
+  "Streets": streets,
+  "Satellite Streets" : satelliteStreets
 };
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/kushallsharma/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/kushallsharma/Mapping_Earthquakes/main/torontoRoutes.json";
+// let torontoData = "https://raw.githubusercontent.com/kushallsharma/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data,{
@@ -54,12 +59,24 @@ L.geoJson(data,{
       onEachFeature: function(feature,layer) {
              //console.log(layer);
              layer.bindPopup()
-             .bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h2> Destination: "+feature.properties.dst + "</h2>")
+             .bindPopup("<h2> Area Code: " + feature.properties.AREA_S_CD + "</h2> <hr> <h2> Area Name: "+feature.properties.AREA_NAME + "</h2>")
            }
     }).addTo(map);
 });
 
-
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data,{
+//       style: myStyle,
+//       onEachFeature: function(feature,layer) {
+//              //console.log(layer);
+//              layer.bindPopup()
+//              .bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h2> Destination: "+feature.properties.dst + "</h2>")
+//            }
+//     }).addTo(map);
+// });
   // Accessing the airport GeoJSON URL
 //   let airportData = "https://raw.githubusercontent.com/kushallsharma/Mapping_Earthquakes/main/majorAirports.json";
 
